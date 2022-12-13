7b88ee2a53d237192d0ba0c342111b437b84bc99
CSS313 <singh.chetan.313@gmail.com>
2022-12-13 10:53:01 +0530

    add add-product page & parse body of form request

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("In the middleware")
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req, res, next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='product'></br><input type='number' name='number'></br><button type='submit'>Add Product</button></form>")
 })
 
-app.use((req, res, next) => {
-    console.log("In another middleware")
-    res.send("<h1>Hello from Express</h1>")
-    // res.send({"key":"value"})
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req, res, next) => {
+    res.send("<h1>Hello from Express!</h1>")
 })
 
 app.listen(3000)
