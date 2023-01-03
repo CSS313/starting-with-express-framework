8cbd6ee2c5b58755f712258c4be72a2ccd30d30b
CSS313 <singh.chetan.313@gmail.com>
2023-01-03 21:32:08 +0530

    change code for task - 6

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
+const path = require('path')
 const express = require('express')
+
+const rootDir = require('../util/path')
 
 const router = express.Router()
 
 router.get('/add-product',(req, res, next) => {
-    res.send(
-        "<form action='/admin/add-product' method='POST'><input type='text' name='product'></br><input type='number' name='number'></br><button type='submit'>Add Product</button></form>"
-    )
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body)
-    res.redirect('/')
+    res.redirect('/shop')
 })
 
 module.exports = router
